54aa340dd01249c085d929960787ae7d10a19526
LIMYUCHEN <2402352@sit.singaporetech.edu.sg>
2026-09-25 15:41:29 +0800

    Restore correct expected value

--- SOFTEST_INTRO_Calculator.UnitTests/CalculatorTests.cs
+++ SOFTEST_INTRO_Calculator.UnitTests/CalculatorTests.cs
@@ -31,7 +31,7 @@ public class CalculatorTests
         // Act
         double result = _calculator.Add(10, 20);
         // Assert
-        Assert.That(result, Is.EqualTo(90));
+        Assert.That(result, Is.EqualTo(30));
     }
 
     // --- Parameterised test with [TestCase] ----------------------------------
